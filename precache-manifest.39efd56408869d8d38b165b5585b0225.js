d15a5408974236c2b3e3300062ec958076042f0d
OtavioBraga0 <otavio.braga123@gmail.com>
2020-03-20 17:57:41 -0300

    Updates

---
+++ precache-manifest.39efd56408869d8d38b165b5585b0225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668dc84bfec63db2c2967218352b0168",
+    "url": "/guiabolso/index.html"
+  },
+  {
+    "revision": "33d0c53752e023b4efbe",
+    "url": "/guiabolso/static/js/2.2333b545.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/guiabolso/static/js/2.2333b545.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5bada3d44a2933f4583",
+    "url": "/guiabolso/static/js/main.6b2f3988.chunk.js"
+  },
+  {
+    "revision": "f4915dd6e668b1498327",
+    "url": "/guiabolso/static/js/runtime-main.2602cfae.js"
+  },
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/guiabolso/static/media/logo.39b94c0e.png"
+  }
+]);
